cd77fe54fb541c12c5a456db8525c41f9f654e6b
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-05 13:47:55 -0700

    add seeds folder with seed.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
+const sequelize = require('./config/connection')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 app.get('/', function(req, res){
     res.send('another day in coding')
 })
 
-app.listen(3000, () => 
-{console.log('look at me')
-
-}
-)
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+      console.log(`http://${PORT}!`);
+    })});
